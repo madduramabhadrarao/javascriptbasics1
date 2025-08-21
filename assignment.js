618712bc57326e90b96913a9292cc35c445cabb9
madduramabhadrarao <83444675+madduramabhadrarao@users.noreply.github.com>
2025-08-21 10:11:33 +0530

    Create assignment.js

---
+++ assignment.js
@@ -0,0 +1,28 @@
+// STUDENT ASSIGNMENT FILE
+// Complete the exercises below
+
+// Exercise 1: Create a variable called 'myName' with your name
+// TODO: Write your code here
+let myName = ""; // Student puts their name here
+
+// Exercise 2: Create a variable called 'myAge' with your age
+// TODO: Write your code here
+let myAge = 0; // Student puts their age here
+
+// Exercise 3: Create a function that adds two numbers
+// TODO: Complete this function
+function addNumbers(a, b) {
+    // Student writes return statement here
+}
+
+// Exercise 4: Create an array with 3 favorite colors
+// TODO: Write your code here
+let favoriteColors = []; // Student fills this array
+
+// DON'T CHANGE THIS - Used for testing
+module.exports = {
+    myName,
+    myAge,
+    addNumbers,
+    favoriteColors
+};
